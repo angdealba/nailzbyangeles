9a6982bbb03dd9833ea547668a32ff4c93dee69e
Angeles de Alba <aid2121@columbia.edu>
2024-10-15 19:20:05 -0400

    Services list page

---
+++ src/Pages/services/services.js
@@ -0,0 +1,129 @@
+import Accordion from 'react-bootstrap/Accordion';
+import { useAccordionButton } from 'react-bootstrap/AccordionButton';
+import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./services.css";
+import {useNavigate} from "react-router-dom";
+import react, {useState} from "react";
+
+export default function Services() {
+    const navigate = useNavigate();
+    const [length, setLength] = useState(null);
+    const [service, setService] = useState(null);
+    const [price, setPrice] = useState(null);
+    const prices = [ [40, 50, 60, 65],[null],[40, 50, 60, 65],[40, 50, 60, 65],[null],[35],[20]];
+
+    function handleSelect(selectedService, selectedLen, selectedPrice){
+        setService(selectedService);
+        setLength(selectedLen)
+        setPrice(selectedPrice);
+        navigate("/appointment", {state: {service: selectedService, length: selectedLen, price: selectedPrice}})
+    }
+
+    return (
+        <>
+
+        <div className="container">
+            <div className="acc-container">
+            <Accordion flush>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>Single Color Set</Accordion.Header>
+                    <Accordion.Body>
+                        <div className="service">
+                            Short Length
+                            <button className="select" onClick={() => handleSelect("Single Color Set", "Short", 40)}>Select</button>
+                        </div>
+                        <div className="service">
+                            Medium Length
+                            <button className="select" onClick={() => handleSelect("Single Color Set", "Medium", 50)}>Select</button>
+                        </div>
+                        <div className="service">
+                            Long Length
+                            <button className="select" onClick={() => handleSelect("Single Color Set", "Long", 60)}>Select</button>
+                        </div>
+                        <div className="service">
+                            XL Long
+                            <button className="select" onClick={() => handleSelect("Single Color Set", "XL", 65)}>Select</button>
+                        </div>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>Inspiration Picture Set</Accordion.Header>
+                    <Accordion.Body>
+                        <div className="service">
+                            <button className="select" onClick={() => handleSelect("Inspiration Picture Set", null, null)}>Select</button>
+                        </div>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header>French Tip Set</Accordion.Header>
+                    <Accordion.Body>
+                        <div className="service">
+                            Short Length
+                            <button className="select" onClick={() => handleSelect("French Tip Set", "Short", 50)}>Select</button>
+                        </div>
+                        <div className="service">
+                            Medium Length
+                            <button className="select" onClick={() => handleSelect("French Tip Set", "Medium", 60)}>Select</button>
+                        </div>
+                        <div className="service">
+                            Long Length
+                            <button className="select" onClick={() => handleSelect("French Tip Set", "Long", 70)}>Select</button>
+                        </div>
+                        <div className="service">
+                            XL Long
+                            <button className="select" onClick={() => handleSelect("French Tip Set", "XL", 75)}>Select</button>
+                        </div>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="3">
+                    <Accordion.Header>Freestyle Set</Accordion.Header>
+                    <Accordion.Body>
+                        <div className="service">
+                            Short Length
+                            <button className="select" onClick={() => handleSelect("Freestyle Set", "Short", 50)}>Select</button>
+                        </div>
+                        <div className="service">
+                            Medium Length
+                            <button className="select" onClick={() => handleSelect("Freestyle Set", "Medium", 60)}>Select</button>
+                        </div>
+                        <div className="service">
+                            Long Length
+                            <button className="select" onClick={() => handleSelect("Freestyle Set", "Long", 70)}>Select</button>
+                        </div>
+                        <div className="service">
+                            XL Long
+                            <button className="select" onClick={() => handleSelect("Freestyle Set", "XL", 75)}>Select</button>
+                        </div>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="4">
+                    <Accordion.Header>Fill-In</Accordion.Header>
+                    <Accordion.Body>
+                        <div className="service">
+                            <button className="select" onClick={() => handleSelect("Fill-In", null, null)}>Select</button>
+                        </div>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="5">
+                    <Accordion.Header>Acrylic Overlay</Accordion.Header>
+                    <Accordion.Body>
+                        <div className="service">
+                            <button className="select" onClick={() => handleSelect("Acrylic Overlay", null, 35)}>Select</button>
+                        </div>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="6">
+                    <Accordion.Header>Gel Manicure</Accordion.Header>
+                    <Accordion.Body>
+                        <div className="service">
+                            <button className="select" onClick={() => handleSelect("Gel Manicure", null, 20)}>Select</button>
+                        </div>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+        </div>
+        </div>
+        </>
+    );
+}
