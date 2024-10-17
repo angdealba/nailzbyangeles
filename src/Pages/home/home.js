1c0ee48cb54b06c1d45d1c6054a75b2a6088a8cb
Angeles de Alba <aid2121@columbia.edu>
2024-10-17 17:05:05 -0400

    Adding navbar to home

--- src/Pages/home/home.js
+++ src/Pages/home/home.js
@@ -1,5 +1,12 @@
 import "./home.css";
-export default function Home() {
+import {useNavigate} from "react-router-dom";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+export default function Home (){
+    const navigate = useNavigate();
+
     return (
         <>
         <div className="home">
@@ -8,6 +15,9 @@ export default function Home() {
             <img src="/images/home/3.png" alt="" />
             <img src="/images/home/4.png" alt="" />
         </div>
+        <div className="book-appt">
+            <button onClick ={() => navigate("/services")} className="btn btn-primary">Book An Appointment Now</button>
+        </div>
         </>
     );
-}
+};
