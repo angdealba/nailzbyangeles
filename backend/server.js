aa675de67c87a51d574d34f0a2b3c62cf5d68088
Angeles de Alba <aid2121@columbia.edu>
2024-10-18 14:21:43 -0400

    Refactoring files. Adding backend. ConnectDB successful

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,13 @@
+const connectDB = require('./db/db_config');
+const dotenv = require('dotenv').config();
+const express = require("express");
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://aid2121:nailzbyangeles@cluster0.vnxtb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+connectDB();
+const app = express();
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
 });
 
-async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-        await listDatabases(client);
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-}
-run().catch(console.dir);
 
-async function listDatabases(client){
-    const databases = await client.db().admin().listDatabases();
-    databases.databases.forEach(database => {console.log(database.name)});
-}
