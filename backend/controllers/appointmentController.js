e712972319c8377a6a89f0ab8257c462296566dc
Angeles de Alba <aid2121@columbia.edu>
2024-10-20 15:16:07 -0400

    Adding async handler to appointment controller

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -1,26 +1,30 @@
+const asyncHandler = require('express-async-handler')
+const Appointments = require('../model/appointmentModel')
+
 // @desc get Appointments
 // @route GET /api/appointments
-const getAppointments = (req, res) => {
-    res.status(200).json({message: 'Get appointments'})
-}
+const getAppointments = asyncHandler (async (req, res) => {
+    const appointments = await Appointments.find()
+    res.status(200).json(appointments)
+})
 
 // @desc create Appointment
 // @route POST /api/appointments
-const createAppointment = (req, res) => {
+const createAppointment = asyncHandler (async (req, res) => {
     res.status(200).json({message: 'Create appointment'})
-}
+})
 
 // @desc update existing Appointment
 // @route PUT /api/appointments/:id
-const updateAppointment = (req, res) => {
+const updateAppointment = asyncHandler (async (req, res) => {
     res.status(200).json({message: `Update appointment ${req.params.id}`})
-}
+})
 
 // @desc delete existing Appointment
 // @route DELETE /api/appointments/:id
-const deleteAppointment = (req, res) => {
+const deleteAppointment = asyncHandler (async (req, res) => {
     res.status(200).json({message: `Delete appointment ${req.params.id}`})
-}
+})
 
 
 
