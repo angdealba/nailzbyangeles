3d1b1220ffe7c56db60b72f5a1f7f38f45f9d97e
Angeles de Alba <aid2121@columbia.edu>
2024-10-21 17:06:43 -0400

    Adding concurrently to run both front and backend simultaneously

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
+
+const dotenv = require('dotenv').config({ path: './backend/.env' });
 const connectDB = require('./db/dbConfig');
-const dotenv = require('dotenv').config();
 const express = require("express");
 
 connectDB();
 const app = express();
 const PORT = process.env.PORT;
 app.use(express.json())
-
+app.use(express.urlencoded())
 
 app.use('/api/appointments', require('./routes/appointmentRoutes'));
 app.use('/api/clients', require('./routes/clientRoutes'));
+console.log(PORT)
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
