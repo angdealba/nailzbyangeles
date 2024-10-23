1d84674fb7efe83910fe714b7507d101d717f649
Angeles de Alba <aid2121@columbia.edu>
2024-10-23 16:03:54 -0400

    Enhancing UI on home page (Book Appointment button).

--- src/Pages/home/home.js
+++ src/Pages/home/home.js
@@ -1,23 +1,27 @@
 import "./home.css";
 import {useNavigate} from "react-router-dom";
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Home (){
     const navigate = useNavigate();
 
     return (
         <>
-        <div className="home">
-            <img src="/images/home/1.png" alt="" />
-            <img src="/images/home/2.png" alt="" />
-            <img src="/images/home/3.png" alt="" />
-            <img src="/images/home/4.png" alt="" />
-        </div>
-        <div className="book-appt">
-            <button onClick ={() => navigate("/services")} className="btn btn-primary">Book An Appointment Now</button>
-        </div>
+            <Col>
+                <div className="home">
+                    <img src="/images/home/1.png" alt="" />
+                    <img src="/images/home/2.png" alt="" />
+                    <img src="/images/home/3.png" alt="" />
+                    <img src="/images/home/4.png" alt="" />
+               </div>
+                <div className="book">
+                    <img src ="/images/home/5.png" />
+                    <button className="book-btn" onClick ={() => navigate("/services")}>
+                        <span>Book Appointment</span>
+                    </button>
+                </div>
+             </Col>
         </>
     );
 };
