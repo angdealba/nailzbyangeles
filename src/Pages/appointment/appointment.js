d1539fd4d90086bf1fcc364ab6e81efcc33dd721
Angeles de Alba <aid2121@columbia.edu>
2024-10-28 23:30:32 -0400

    Modifying UI for appointment page

--- src/Pages/appointment/appointment.js
+++ src/Pages/appointment/appointment.js
@@ -1,7 +1,7 @@
 import "./appointment.css";
 import {useState} from "react";
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import './customCalendar.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import {useNavigate} from "react-router-dom";
@@ -15,7 +15,6 @@ export default function Appointment(){
     function handleClickDay(value){
         setDate(value);
         sessionStorage.setItem("date", value);
-
     }
 
     function handleSelect(time){
@@ -26,15 +25,21 @@ export default function Appointment(){
     return (
         <>
             <Row>
-                <Col>
-                    <h6>{sessionStorage.getItem("service")}  ({sessionStorage.getItem("length")}) - ${sessionStorage.getItem("price")}</h6>
-                    <Calendar onClickDay={handleClickDay}></Calendar>
+                <Col className="calendar">
+                    <h5>Select a Date & Time</h5>
+                    <h7>
+                        {sessionStorage.getItem("service")}
+                        {sessionStorage.getItem("length") && sessionStorage.getItem("length") !== "null" ?
+                            ` (${sessionStorage.getItem("length")}) ` : ""}
+                        - ${sessionStorage.getItem("price")}
+                    </h7>
+                    <Calendar defaultValue={new Date()} minDate={new Date()} onClickDay={handleClickDay}></Calendar>
                 </Col>
-                <Col>
-                    <h6>{dayFormatter.format(date)}, {monthFormatter.format(date)} {date.getDate()}</h6>
-                    <button onClick={() => handleSelect("9")}>9:00AM</button>
-                    <button onClick={() => handleSelect("10")}>10:00AM</button>
-                    <button onClick={() => handleSelect("11")}>11:00AM</button>
+                <Col className="availability">
+                    <h5>{dayFormatter.format(date)}, {monthFormatter.format(date)} {date.getDate()}</h5>
+                    <button className="time" onClick={() => handleSelect("9")}>9:00AM</button>
+                    <button className="time" onClick={() => handleSelect("10")}>10:00AM</button>
+                    <button className="time" onClick={() => handleSelect("11")}>11:00AM</button>
                 </Col>
             </Row>
         </>
