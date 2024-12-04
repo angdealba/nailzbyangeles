5c2bc2708a94b10b23dac2daa343cd58668b2b04
Angeles de Alba <aid2121@columbia.edu>
2024-12-04 15:57:24 -0500

    Error checking createPaymentIntent

--- backend/controllers/stripeController.js
+++ backend/controllers/stripeController.js
@@ -4,14 +4,23 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {});
 // @desc create PaymentIntent returns client_secret
 // @route POST /api/stripe/create-payment-intent
 const createPaymentIntent = asyncHandler(async (req, res) => {
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: 1000,
-        currency: 'usd',
-        automatic_payment_methods: {
-            enabled: true,
-        },
-    });
-    res.send({clientSecret: paymentIntent.client_secret});
+    try{
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: 1000,
+            currency: 'usd',
+            automatic_payment_methods: {
+                enabled: true,
+            },
+        });
+        res.send({clientSecret: paymentIntent.client_secret});
+    }
+    catch (e){
+        return res.status(400).send({
+            error: {
+                message: e.message,
+            },
+        });
+    }
 })
 
 // @desc send STRIPE_PUBLISHABLE_KEY to client
--- src/Pages/payment/payment.js
+++ src/Pages/payment/payment.js
@@ -8,27 +8,29 @@ export default function Payment (){
     const [stripePromise, setStripePromise] = useState(null)
     const [clientSecret, setClientSecret] = useState("")
 
+
     useEffect(() => {
         stripeService.configStripe().then(async (r) => {
-            console.log(r.json)
-            const {publishableKey} = await r.json;
-            setStripePromise(loadStripe(publishableKey))
+            const {publishableKey} = await r;
+            const stripe = await loadStripe(publishableKey);
+            setStripePromise(stripe);
         })
     }, [])
 
     useEffect(() => {
         stripeService.createPaymentIntent(JSON.stringify({})).then(async (result) => {
             const {clientSecret} = await result;
-            console.log(clientSecret)
             setClientSecret(clientSecret);
         });
     }, []);
 
+
     return (
         <>
+
             <h5>To finalize your booking, all appointments require a $10 deposit. The deposit will count towards the remaining balance. </h5>
             {clientSecret && stripePromise && (
-                <Elements stripe={stripePromise} options={clientSecret}>
+                <Elements stripe={stripePromise} options={{clientSecret}}>
                     <CheckoutForm />
                 </Elements>
             )}
