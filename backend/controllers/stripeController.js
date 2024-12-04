655bcaf55b75e0640a1b0e2768e9d63f012fc59e
Angeles de Alba <aid2121@columbia.edu>
2024-12-04 15:03:07 -0500

    Send clientSecret and publishableKey as json. Adding async handler

--- backend/controllers/stripeController.js
+++ backend/controllers/stripeController.js
@@ -3,22 +3,22 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {});
 
 // @desc create PaymentIntent returns client_secret
 // @route POST /api/stripe/create-payment-intent
-const createPaymentIntent = (req, res) => {
-    const paymentIntent = stripe.paymentIntents.create({
-        amount: 10,
+const createPaymentIntent = asyncHandler(async (req, res) => {
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: 1000,
         currency: 'usd',
         automatic_payment_methods: {
             enabled: true,
         },
     });
-    res.status(201).json(paymentIntent.client_secret);
-}
+    res.send({clientSecret: paymentIntent.client_secret});
+})
 
 // @desc send STRIPE_PUBLISHABLE_KEY to client
 // @route GET /api/stripe/config
-const sendPublishableKey = asyncHandler (async (req, res) => {
-    res.status(200).json(process.env.STRIPE_PUBLISHABLE_KEY);
-})
+const sendPublishableKey =  (req, res) => {
+    res.send({publishableKey: process.env.STRIPE_PUBLISHABLE_KEY});
+}
 
 module.exports = {
     createPaymentIntent, sendPublishableKey
