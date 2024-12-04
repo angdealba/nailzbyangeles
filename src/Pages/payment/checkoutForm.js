1c72bfc95172ba0a2977faf63ef8e6f60c7c7417
Angeles de Alba <aid2121@columbia.edu>
2024-12-04 15:55:21 -0500

    Styling and error checking Stripe checkoutForm

--- src/Pages/payment/checkoutForm.js
+++ src/Pages/payment/checkoutForm.js
@@ -1,18 +1,36 @@
 import { PaymentElement } from "@stripe/react-stripe-js";
 import { useStripe, useElements } from "@stripe/react-stripe-js";
+import {useState} from "react";
+import "./checkoutForm.css"
 
 export default function CheckoutForm() {
     const stripe = useStripe();
     const elements = useElements();
+    const [message, setMessage] = useState(null);
+    const [isProcessing, setIsProcessing] = useState(false);
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        setIsProcessing(true);
+        const { error } = await stripe.confirmPayment({
+            elements,
+            confirmParams: {
+                return_url: `${window.location.origin}/confirmation`,
+            },
+        });
+        setIsProcessing(false);
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <PaymentElement />
-            <button disabled={!stripe}>Submit</button>
+        <form id="payment-form" onSubmit={handleSubmit}>
+            <PaymentElement id="payment-element" />
+            <button className = "pay-now" disabled={isProcessing || !stripe || !elements} id="submit">
+        <span id="button-text">
+          {isProcessing ? "Processing ... " : "Pay now"}
+        </span>
+            </button>
         </form>
     );
 }
