b54c2eb293793e25fe72c21fb3d87cb9ae32269d
Angeles de Alba <aid2121@columbia.edu>
2024-12-07 16:32:44 -0500

    Configuring redux-persist to use session storage

--- src/App/store.js
+++ src/App/store.js
@@ -1,9 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit'
-import bookingReducer from '../features/bookingSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from "redux-persist";
+import storageSession from 'redux-persist/lib/storage/session';
+import bookingReducer from '../Features/bookingSlice';
 
+
+const persistConfig = {
+    key: "root",
+    storage: storageSession,
+};
+
+const persistedReducer = persistReducer(persistConfig, bookingReducer);
 export const store = configureStore({
-    reducer: {
-        booking: bookingReducer,
-    },
-})
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false
+        }),
+});
+
+export const persistor = persistStore(store);
+export default store;
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React, { StrictMode } from "react";
+import { Provider } from 'react-redux';
 import { createRoot } from "react-dom/client";
-
+import {persistor, store} from './App/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-    <StrictMode>
-        <App />
-    </StrictMode>
+    <React.StrictMode>
+        <Provider store = {store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>
 );
+
